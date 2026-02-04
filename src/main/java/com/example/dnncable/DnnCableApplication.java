86007cf5ab5af3c3722c9c5c8f51af61a2d2ab97
kant300 <p.o.i.u.0987552@gmail.com>
2026-02-04 09:23:16 +0900

    index페이지

---
+++ src/main/java/com/example/dnncable/DnnCableApplication.java
@@ -0,0 +1,13 @@
+package com.example.dnncable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DnnCableApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DnnCableApplication.class, args);
+    }
+
+}
